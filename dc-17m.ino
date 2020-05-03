06f83f05b3e0419a625b3e91b493e9a7f08e44e4
AlexBrandt46 <ajb3155@gmail.com>
2020-05-03 19:29:29 -0400

    First commit

---
+++ dc-17m.ino
@@ -0,0 +1,12 @@
+const int totalAmmo = 300;      // The max ammo the gun hold before resetting
+int magAmmo = 60;               // The ammo count of the magazine currently in the gun
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
