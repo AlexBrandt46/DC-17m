c92f8373abf7ad026a298b3098825d5ed0d83320
AlexBrandt46 <ajb3155@gmail.com>
2020-05-05 16:50:13 -0400

    Added button to handle reloading the gun whenever the user wants

--- dc-17m.ino
+++ dc-17m.ino
@@ -1,9 +1,10 @@
 // 60 rounds/magazine
 // 300 rounds max in gun
 
-const int buttonPin = 8;
+const int firePin = 8;
 const int ledPin = 9;
 const int buzzPin = 7;
+const int reloadPin = 6;
 
 int firingState = 0;
 
@@ -14,14 +15,15 @@ void setup() {
   Serial.begin(115200);
   Serial.println("starting");
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(firePin, INPUT_PULLUP);
   pinMode(buzzPin, OUTPUT);
+  pinMode(reloadPin, INPUT_PULLUP);
 }
 
 void loop() {
 
   // Read the button state for the trigger button (pressed or not pressed)
-  firingState = digitalRead(buttonPin);
+  firingState = digitalRead(firePin);
 
   // Check that there's still ammo in the current magazine and in the gun
   if (firingState == HIGH && magAmmo > 0 && totalAmmo > 0) {
@@ -29,27 +31,34 @@ void loop() {
     Serial.println(magAmmo);
     digitalWrite(ledPin, HIGH);
     digitalWrite(buzzPin, HIGH);
-    delay(100);
+    delay(50);
   } 
   else if (firingState == LOW) {
     digitalWrite(ledPin, LOW);
     digitalWrite(buzzPin, LOW);
-  }
 
-  // Do auto reload for now only if the magazine's completely empty
-  if (magAmmo == 0) {
-
-    // Check whether to remove a full magazine from the total ammo left or just what's left if there's less than a full magazine left
-    if (totalAmmo >= 60) {
-      magAmmo = 60;           // Add 60 rounds to the magazine
-      totalAmmo -= 60;        // Take 60 rounds out of the remaining count
-    }
-    else if (totalAmmo != 0) {
-      magAmmo = totalAmmo;
-      totalAmmo = 0;
+    if (digitalRead(reloadPin) == HIGH) {
+      reload();
+      Serial.println(totalAmmo);
+      Serial.println(magAmmo);
     }
   }
 
   delay(100);
+  
+}
+
+void reload() {
+  // Check whether to remove a full magazine from the total ammo left or just what's left if there's less than a full magazine left
+  if (totalAmmo >= 60) {
+    totalAmmo -= (60 - magAmmo);
+    magAmmo = 60;           // Add 60 rounds to the magazine
+  }
+  else if (totalAmmo != 0) {
+    magAmmo = totalAmmo;
+    totalAmmo = 0;
+  }
+
+  delay(300);
   
 }
